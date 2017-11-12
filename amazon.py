6a3837f2fadeeb71e50992c0d13d0ad09c5385f6
Aitor Cubeles Torres <aitorct7@gmail.com>
2017-11-12 03:34:23 +0100

    Compatibility with other Amazon templates

--- amazon.py
+++ amazon.py
@@ -3,21 +3,31 @@ from lxml import html
 import re
 import string
 
-def checkPrice(url):
-  response = requests.get(url)
-  while(response.status_code != 200):
-    response = requests.get(url)
-  website = html.fromstring(response.content)
-  
-  price = website.xpath("//span[@id='priceblock_saleprice']/text()")[0]
 
-  return re.sub("[^0123456789\.,]", "", price)
+def checkPrice(url):
+    response = requests.get(url)
+    while(response.status_code != 200):
+        response = requests.get(url)
+    website = html.fromstring(response.content)
+    price = None
+    
+    try:
+        price = website.xpath("//span[@id='priceblock_saleprice']/text()")[0]
+    except IndexError:
+        pass
+
+    try:
+        price = website.xpath("//span[@id='priceblock_ourprice']/text()")[0]
+    except IndexError:
+        pass
+
+    return re.sub("[^0123456789\.,]", "", price)
+
 
 def getName(url):
-  response = requests.get(url)
-  while(response.status_code != 200):
     response = requests.get(url)
-  website = html.fromstring(response.content)
+    while(response.status_code != 200):
+        response = requests.get(url)
+    website = html.fromstring(response.content)
 
-  return website.xpath("//span[@id='productTitle']/text()")[0]
-
+    return website.xpath("//span[@id='productTitle']/text()")[0]
